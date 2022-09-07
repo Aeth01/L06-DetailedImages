ee7677cb326b204ec6272b447a8ed24a97d8f701
Aethalis <icelasersparr@gmail.com>
2022-09-08 01:40:16 +1000

    Now using view binding (this is awesome), and passing data using intents. Parcelize does not work for me.

--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/DetailActivity.kt
@@ -2,11 +2,21 @@ package au.edu.swin.sdmd.l05_detailedimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import au.edu.swin.sdmd.l05_detailedimages.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val nameStr = intent.getStringExtra("name")
+        binding.name.text = nameStr
+
+        val authorStr = intent.getStringExtra("author")
+        binding.author.text = authorStr
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l05_detailedimages/MainActivity.kt
@@ -1,20 +1,33 @@
 package au.edu.swin.sdmd.l05_detailedimages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import au.edu.swin.sdmd.l05_detailedimages.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val station = Location("Glenferrie Station", "Ada May Plante",
             -37.821539f, 145.036473f)
 
         val vStation = findViewById<TextView>(R.id.station)
         vStation.setOnClickListener {
-
+            val detailActivityIntent = Intent(this, DetailActivity::class.java)
+            detailActivityIntent.putExtra("name", station.name)
+            detailActivityIntent.putExtra("author", station.author)
+            detailActivityIntent.putExtra("latitude", station.latitude)
+            detailActivityIntent.putExtra("longitude", station.longitude)
+//            detailActivityIntent.putExtra("location", station)
+            startActivity(detailActivityIntent)
         }
     }
 
